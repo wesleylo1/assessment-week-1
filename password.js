3b3e655fd002c92232a83651e822f9abb8341c80
Wesley Lo <wesleylo1995@gmail.com>
2022-01-07 10:44:18 -0600

    initial commit for password.js

---
+++ password.js
@@ -0,0 +1,12 @@
+// Welcome the user to the password validator tool
+
+// Prompt the user for a password to validate
+
+// Check if the user’s password meets the following constraint:
+
+// At least 10 characters long
+
+// If the user’s password meets the constraint, show a success message to the user
+
+// If the user’s password fails the constraint, show a failure message to the user
+
