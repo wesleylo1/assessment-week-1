aa189c74505a64950aa14db0740b1e8eae48ec66
Wesley Lo <wesleylo1995@gmail.com>
2022-01-07 11:22:53 -0600

    removed comments in password.js

--- password.js
+++ password.js
@@ -1,15 +1,3 @@
-// Welcome the user to the password validator tool
-
-// Prompt the user for a password to validate
-
-// Check if the user’s password meets the following constraint:
-
-// At least 10 characters long
-
-// If the user’s password meets the constraint, show a success message to the user
-
-// If the user’s password fails the constraint, show a failure message to the user
-
 const readline = require('readline');
 
 const reader = readline.createInterface({
@@ -31,7 +19,7 @@ if (password.length >= 10) {
 } else {
     console.log('failure')
 }
-	//This line closes the connection to the command line interface.
+	
 	reader.close()
 
 });
