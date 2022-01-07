3b5f118b13cf37990af9b6c241b3980bb0f807e1
Wesley Lo <wesleylo1995@gmail.com>
2022-01-07 11:21:21 -0600

    Password complete

--- password.js
+++ password.js
@@ -10,3 +10,29 @@
 
 // If the user’s password fails the constraint, show a failure message to the user
 
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+console.log('Welcome to the password validator tool!')
+
+reader.question("Now...what is the password?", function(input){
+	tokens = input.split(' ');
+	
+	let password = tokens[0];
+	
+
+if (password.length >= 10) {
+    console.log('success')
+} else {
+    console.log('failure')
+}
+	//This line closes the connection to the command line interface.
+	reader.close()
+
+});
+
